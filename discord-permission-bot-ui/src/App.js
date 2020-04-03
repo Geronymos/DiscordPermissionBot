80a46b9038fbfd3eea262b81556e5b6fb4ab57a3
Geronymos <spielerbereich@gmail.com>
2020-04-04 01:29:33 +0200

    Frontend show channels with roles in table

--- discord-permission-bot-ui/src/App.js
+++ discord-permission-bot-ui/src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { bot_url, server_id } from './config.json';
+import { Container, Table } from 'react-bootstrap';
 
 function App() {
+  var [data, setData] = React.useState({channels: [], roles: []});
+
+  React.useEffect( () => {
+    async function fetchData() {
+      const response = await fetch( bot_url + "/?server=" + server_id);
+      const result = await response.json();
+      setData(result)
+    }
+    fetchData();
+  }, [] );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Table>
+        <thead>
+          <tr>
+            <th>Channel</th>
+            <th>Roles</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.channels.map( (channel, key) => 
+            <tr key={key} >
+              <td>{channel.name}</td>
+              <td>{ channel.permission_overwrites.map( overwride => data.roles.find( role => role.id === parseInt(overwride.id) ).name ).join(", ") }</td>
+            </tr>
+          )}
+          <tr>
+          </tr>
+          <tr></tr>
+        </tbody>
+      </Table>
+    </Container>
   );
 }
 
