1e415c7be5b84053b864bf15bbde84c249555fd8
Geronymos <spielerbereich@gmail.com>
2020-04-04 02:14:36 +0200

    colored roles

--- discord-permission-bot-ui/src/App.js
+++ discord-permission-bot-ui/src/App.js
@@ -2,19 +2,25 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { bot_url, server_id } from './config.json';
-import { Container, Table } from 'react-bootstrap';
+import { Container, Table, Badge, Button } from 'react-bootstrap';
 
 function App() {
-  var [data, setData] = React.useState({channels: [], roles: []});
+  var [data, setData] = React.useState({ channels: [], roles: [] });
 
-  React.useEffect( () => {
+  React.useEffect(() => {
     async function fetchData() {
-      const response = await fetch( bot_url + "/?server=" + server_id);
+      const response = await fetch(bot_url + "/?server=" + server_id);
       const result = await response.json();
       setData(result)
     }
     fetchData();
-  }, [] );
+  }, []);
+
+  function channelGetRoles(channels, channel) {
+    const roleIDs = channel.permission_overwrites.map(overwrite => parseInt(overwrite.id));
+    const roles = data.roles.filter(role => roleIDs.includes(role.id));
+    return roles;
+  }
 
   return (
     <Container>
@@ -26,10 +32,15 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {data.channels.map( (channel, key) => 
+          {data.channels.map((channel, key) =>
             <tr key={key} >
               <td>{channel.name}</td>
-              <td>{ channel.permission_overwrites.map( overwride => data.roles.find( role => role.id === parseInt(overwride.id) ).name ).join(", ") }</td>
+              <td>{channelGetRoles(data.channels, channel).map(role => {
+                const color = "#" + (role.color || 8948369).toString(16);
+                // return <Button variant="outline-primary" className="mr-1" size="sm" style={{borderColor: color, color}}>{role.name}</Button>
+                return <Badge className="mr-1" variant="primary" style={{ background: color }}>{role.name}</Badge>
+              })
+              }</td>
             </tr>
           )}
           <tr>
