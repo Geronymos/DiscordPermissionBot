a76a3bb048496cdf9045139972fce399063bb426
Geronymos <spielerbereich@gmail.com>
2020-04-17 15:07:07 +0200

    cli tools: change permissions of a role of a channel

---
+++ .cli-tools/role-channels.php
@@ -0,0 +1,30 @@
+<?php
+
+include('../vendor/autoload.php');
+
+use RestCord\DiscordClient;
+
+// for use in console (PHP cli)
+if (isset($argc)) {
+    $options = getopt("c:r:a:d:t:");
+    $discord = new DiscordClient(['token' => $options['t']]); // Token is required
+
+    role_channel(
+        $discord,
+        $options['c'], // guild
+        $options['r'], // role
+        $options['a'], // allow
+        $options['d'] // deny
+    );
+}
+
+function role_channel($client, $channelID, $roleID, $allow, $deny) {
+    $parameters = [
+        "channel.id" => intval($channelID),
+        "overwrite.id" => strval($roleID),
+        "allow" => intval($allow),
+        "deny" => intval($deny),
+        "type" => "role"
+    ];
+    $client->channel->editChannelPermissions($parameters);
+}
